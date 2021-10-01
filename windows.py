49580da4f687cd5d86cffb39db4417826f76cb33
Bomzz06 <91737103+Bomzz06@users.noreply.github.com>
2021-10-01 19:42:23 +0700

    Add files via upload

---
+++ windows.py
@@ -0,0 +1,76 @@
+import os
+import pafy
+
+path = "C:/Python Downloader"
+
+if not FileExistsError(path):
+    os.mkdir(path)
+
+if not os.path.exists(path):
+    os.makedirs(path)
+else:
+    print("Please Wait! Delay : 2s")
+
+import time
+time.sleep(2)
+os.chdir("C:/Python Downloader")
+print("Download file auto located to : {0}".format(os.getcwd()))
+
+print("Python Downloader Tools")
+def mainMenu():
+    print("1 - .mp4 Youtube Downloader")
+    print("2 - .mp3 Youtube Downloader")
+    print("3 - EXIT")
+
+mainMenu()
+option = int(input("Write down the numbers you want to choose : "))
+
+def MP4():
+    mp4downloader = input("Paste your link here : ")
+    url = pafy.new(mp4downloader)
+
+    print("Video Title : "+url.title)
+    print("Please wait ...")
+    import time
+    time.sleep(2)
+
+    result = url.getbest()
+    result.download(f"{url.title}.mp4")
+    import time
+    time.sleep(2)
+    print("Please Wait! Delay : 2s")
+    mainMenu()
+
+def MP3():
+    mp3downloader = input("Paste your link here : ")
+    url = pafy.new(mp3downloader)
+
+    print("Video Title : "+url.title)
+    print("Please Wait! Delay : 2s")
+    import time
+    time.sleep(2)
+
+    result = url.getbestaudio()
+    result.download(f"{url.title}.mp3")
+    import time
+    time.sleep(2)
+    print("Please Wait! Delay : 2s")
+    mainMenu()
+
+while option != 0:
+    if option == 1: 
+        MP4()
+    elif option == 2: 
+        MP3()
+    elif option == 3:
+        sure = input("Are you sure? Y/N : ")
+        if sure == "Y": break 
+        elif sure == "N": print("Please Wait! Delay : 2s")
+        import time
+        time.sleep(2)
+        mainMenu()
+    else: print("Invalid Number!")
+
+    option = int(input("Write down the numbers you want to choose : "))
+
+print("Thank you for using this tools!")
